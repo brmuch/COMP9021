e783b983d27a209f9a1a52163398ef43dc6cdf56
brmuch <37830532+brmuch@users.noreply.github.com>
2018-11-06 16:22:43 +1100

    Update sample_3.py

--- Final sample/sample_3.py
+++ Final sample/sample_3.py
@@ -2,13 +2,10 @@
 Given a word w, a good subsequence of w is defined as a word w' such that
 - all letters in w' are different;
 - w' is obtained from w by deleting some letters in w.
-
 Returns the list of all good subsequences, without duplicates, in lexicographic order
 (recall that the sorted() function sorts strings in lexicographic order).
-
 The number of good sequences grows exponentially in the number of distinct letters in w,
 so the function will be tested only for cases where the latter is not too large.
-
 '''
 
 from itertools import combinations
@@ -50,7 +47,7 @@ def good_subsequences(word):
             index = i
             flag.append(i)
     result = []
-    for i in range(0, 4):
+    for i in range(0, len(set(flag)) + 1):
         for j in combinations(flag, i):
             if is_not_two_equal(j):
                 result.append(j)
