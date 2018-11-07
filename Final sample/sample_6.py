131f7bbc838a7f8d907aa0c72b8d5ba9849b93db
brmuch <37830532+brmuch@users.noreply.github.com>
2018-11-07 21:09:00 +1100

    Update sample_6.py

--- Final sample/sample_6.py
+++ Final sample/sample_6.py
@@ -1,18 +1,14 @@
 '''
 is_valid_prefix_expression(expression) checks whether the string expression
 represents a correct infix expression (where arguments follow operators).
-
 evaluate_prefix_expression(expression) returns the result of evaluating expression.
-
 For expression to be syntactically correct:
 - arguments have to represent integers, that is, tokens that can be converted to an integer
   thanks to int();
 - operators have to be any of +, -, * and /;
 - at least one space has to separate two consecutive tokens.
-
 Assume that evaluate_prefix_expression() is only called on syntactically correct expressions,
 and that / (true division) is applied to a denominator that is not 0.
-
 You might find the reversed() function, the split() string method,
 and the pop() and append() list methods useful.
 '''
@@ -66,13 +62,17 @@ def is_valid_prefix_expression(expression):
         for i in range(0, len(list)):
             if list[i] not in operation:
                 list[i] = int(list[i])
-        nb_operation = 0
-        for i in list:
-            if i in operation:
-                nb_operation += 1
-        if len(list) - nb_operation != nb_operation + 1:
+        nb = 0
+        for i in range(0, len(list))[::-1]:
+            if list[i] not in ['*', '/', '-', '+']:
+                nb += 1
+            else:
+                nb = nb - 1
+            if nb == 0:
+                raise IndexError
+        if nb != 1:
             raise IndexError
-        # Replace pass above with your code
+    # Replace pass above with your code
     # - IndexError is raised in particular when trying to pop from an empty list
     # - ValueError is raised in particular when trying to convert to an int
     #   a string that cannot be converted to an int
